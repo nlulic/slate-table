8f9c286ec5274561d17ec652eec190f8d37da2ac
nlulic <nlulic@users.noreply.github.com>
2023-04-05 22:39:17 +0200

    wip: normalize table

--- src/with-table.ts
+++ src/with-table.ts
@@ -1,16 +1,76 @@
 import { EDITOR_TO_WITH_TABLE_OPTIONS } from "./weak-maps";
 import { WithTableOptions, DEFAULT_WITH_TABLE_OPTIONS } from "./options";
-import { mergeDeep } from "./utils";
-import { type Editor } from "slate";
+import { Editor, Element, Node, Transforms } from "slate";
+import { isElement, isOfType } from "./table-editor";
 
 export const withTable = <T extends Editor>(
   editor: T,
   options: Partial<WithTableOptions>
 ): T => {
-  EDITOR_TO_WITH_TABLE_OPTIONS.set(
-    editor,
-    mergeDeep(DEFAULT_WITH_TABLE_OPTIONS, options)
-  );
+  const withTableOptions: WithTableOptions = {
+    blocks: { ...DEFAULT_WITH_TABLE_OPTIONS.blocks, ...options.blocks },
+  };
+
+  EDITOR_TO_WITH_TABLE_OPTIONS.set(editor, withTableOptions);
+
+  const { normalizeNode } = editor;
+
+  const { blocks } = withTableOptions;
+  const ALLOWED_TABLE_CHILDREN = [blocks.thead, blocks.tbody, blocks.tfoot];
+
+  editor.normalizeNode = (entry) => {
+    const [node, path] = entry;
+
+    if (isElement(node) && node.type === blocks.table) {
+      for (const [child, childPath] of Node.children(editor, path)) {
+        if (isElement(child) && ALLOWED_TABLE_CHILDREN.includes(child.type)) {
+          continue;
+        }
+
+        const [tbody] = Editor.nodes(editor, {
+          match: isOfType(editor, "tbody"),
+          at: path,
+        });
+
+        if (!tbody) {
+          return Transforms.wrapNodes(
+            editor,
+            {
+              type: blocks.tbody,
+              children: [child],
+            } as Element,
+            {
+              at: childPath,
+            }
+          );
+        }
+
+        const [tbodyElement, tbodyPath] = tbody;
+        const elements = tbodyElement.children.filter(
+          (n) => isElement(n) && !editor.isInline(n)
+        );
+
+        return Transforms.moveNodes(editor, {
+          at: childPath,
+          to: [...tbodyPath, elements.length],
+        });
+      }
+    }
+
+    if (isElement(node) && node.type === blocks.thead) {
+      // TODO:
+    }
+
+    if (isElement(node) && [blocks.tbody, blocks.tfoot].includes(node.type)) {
+      // TODO:
+    }
+
+    if (isElement(node) && node.type === blocks.content) {
+      // TODO:
+    }
+
+    normalizeNode(entry);
+  };
 
   return editor;
 };
---
+++ test/normalization/normalize-table.test.tsx
@@ -0,0 +1,185 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+
+import assert from "assert";
+import { DEFAULT_WITH_TABLE_OPTIONS } from "../../src/options";
+import { Editor } from "slate";
+import { jsx, withTest } from "../index";
+import { withTable } from "../../src/with-table";
+
+describe("normalize table element", () => {
+  it("should wrap invalid table children into existing `tbody`", () => {
+    const actual = (
+      <editor>
+        <table>
+          <paragraph>
+            <text />
+          </paragraph>
+          <tbody></tbody>
+        </table>
+      </editor>
+    );
+
+    const expected = (
+      <editor>
+        <table>
+          <tbody>
+            <paragraph>
+              <text />
+            </paragraph>
+          </tbody>
+        </table>
+      </editor>
+    );
+
+    const editor = withTest(withTable(actual, DEFAULT_WITH_TABLE_OPTIONS));
+
+    Editor.normalize(editor, { force: true });
+
+    assert.deepEqual(editor.children, expected.children);
+  });
+
+  it("should wrap invalid table children into existing `tbody` and remove existing inline elements", () => {
+    const actual = (
+      <editor>
+        <table>
+          <paragraph>
+            <text />
+          </paragraph>
+          <tbody>
+            <inline>1</inline>
+          </tbody>
+        </table>
+      </editor>
+    );
+
+    const expected = (
+      <editor>
+        <table>
+          <tbody>
+            <paragraph>
+              <text />
+            </paragraph>
+          </tbody>
+        </table>
+      </editor>
+    );
+
+    const editor = withTest(withTable(actual, DEFAULT_WITH_TABLE_OPTIONS));
+
+    Editor.normalize(editor, { force: true });
+
+    assert.deepEqual(editor.children, expected.children);
+  });
+
+  it("should wrap table children into existing `tbody` in the correct order", () => {
+    const actual = (
+      <editor>
+        <table>
+          <paragraph>
+            <text>1</text>
+          </paragraph>
+          <paragraph>
+            <text>2</text>
+          </paragraph>
+          <paragraph>
+            <text>3</text>
+          </paragraph>
+          <tbody></tbody>
+        </table>
+      </editor>
+    );
+
+    const expected = (
+      <editor>
+        <table>
+          <tbody>
+            <paragraph>
+              <text>1</text>
+            </paragraph>
+            <paragraph>
+              <text>2</text>
+            </paragraph>
+            <paragraph>
+              <text>3</text>
+            </paragraph>
+          </tbody>
+        </table>
+      </editor>
+    );
+
+    const editor = withTest(withTable(actual, DEFAULT_WITH_TABLE_OPTIONS));
+
+    Editor.normalize(editor, { force: true });
+
+    assert.deepEqual(editor.children, expected.children);
+  });
+
+  it("should wrap invalid table children into existing `tbody` with multiple children", () => {
+    const actual = (
+      <editor>
+        <table>
+          <paragraph>
+            <text />
+          </paragraph>
+          <tbody>
+            <tr>
+              <text />
+            </tr>
+          </tbody>
+        </table>
+      </editor>
+    );
+
+    const expected = (
+      <editor>
+        <table>
+          <tbody>
+            <tr>
+              <text />
+            </tr>
+            <paragraph>
+              <text />
+            </paragraph>
+          </tbody>
+        </table>
+      </editor>
+    );
+
+    const editor = withTest(withTable(actual, DEFAULT_WITH_TABLE_OPTIONS));
+
+    Editor.normalize(editor, { force: true });
+
+    assert.deepEqual(editor.children, expected.children);
+  });
+
+  it("should create `tbody` if it doesn't exist and wrap invalid table children into it", () => {
+    const actual = (
+      <editor>
+        <table>
+          <paragraph>
+            <text />
+          </paragraph>
+        </table>
+      </editor>
+    );
+
+    const expected = (
+      <editor>
+        <table>
+          <tbody>
+            <paragraph>
+              <text />
+            </paragraph>
+          </tbody>
+        </table>
+      </editor>
+    );
+
+    const editor = withTest(withTable(actual, DEFAULT_WITH_TABLE_OPTIONS));
+
+    Editor.normalize(editor, { force: true });
+
+    assert.deepEqual(editor.children, expected.children);
+  });
+});
